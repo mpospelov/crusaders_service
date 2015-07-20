b2d8a3309f2bdf067075dcf77b2f4db02cd85f39
unknown <S14004730@STU007128W.wrexham.local>
2015-07-20 11:26:28 +0100

    add tickets

--- CrusadersService/CrusadersService.svc.cs
+++ CrusadersService/CrusadersService.svc.cs
@@ -10,8 +10,12 @@ namespace CrusadersService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IService1
+    public class CrusadersService : ICrusadersService
     {
+        static CrusadersEntities db = new CrusadersEntities();
+
+
+
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
@@ -28,6 +32,33 @@ namespace CrusadersService
                 composite.StringValue += "Suffix";
             }
             return composite;
+        }
+
+
+        public List<Ticket> GetAllTickets()
+        {
+            return db.Tickets.ToList();
+        }
+
+        public Ticket GetTicket(int id)
+        {
+            return db.Tickets.Find(id);
+        }
+
+        public void CreateTicket(Ticket ticket)
+        {
+            db.Tickets.Add(ticket);
+            db.SaveChanges();
+        }
+
+        public void UpdateTicket(int id, Ticket ticket)
+        {
+            Ticket updatedTicket = db.Tickets.Find(id);
+            updatedTicket.Description = ticket.Description;
+            updatedTicket.Game_id = ticket.Game_id;
+            updatedTicket.Price = ticket.Price;
+            updatedTicket.Type = ticket.Type;
+            db.SaveChanges();
         }
     }
 }
--- CrusadersService/ICrusadersService.cs
+++ CrusadersService/ICrusadersService.cs
@@ -10,7 +10,7 @@ namespace CrusadersService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface ICrusadersService
     {
 
         [OperationContract]
@@ -19,7 +19,17 @@ namespace CrusadersService
         [OperationContract]
         CompositeType GetDataUsingDataContract(CompositeType composite);
 
-        // TODO: Add your service operations here
+        [OperationContract]
+        List<Ticket> GetAllTickets();
+
+        [OperationContract]
+        Ticket GetTicket(int id);
+
+        [OperationContract]
+        void CreateTicket(Ticket ticket);
+
+        [OperationContract]
+        void UpdateTicket(int id, Ticket ticket);
     }
 
 
@@ -44,4 +54,5 @@ namespace CrusadersService
             set { stringValue = value; }
         }
     }
+
 }
